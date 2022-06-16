a516b9cb1c5b6ff532b61bad6b1672b67334845c
Maxgreen2000 <97794858+Maxgreen2000@users.noreply.github.com>
2022-06-16 11:37:20 +0100

    All lecture 2 materials covered in the lecture.

---
+++ lecture2/classes.py
@@ -0,0 +1,25 @@
+class Flight():
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.passengers = []
+    
+    def add_passengers(self, name):
+        if not self.open_seats():  ## this effectively says if open_seats() == 0
+            return False
+        self.passengers.append(name)
+        return True
+        
+    def open_seats(self):
+        return self.capacity - len(self.passengers)
+
+        
+flight = Flight(3)
+
+people = ["Harry", "Ron", "Hermione", "Ginny"]
+for person in people:
+    if flight.add_passengers(person):
+        print(f"Added {person} to flight succesfully.")
+    else:
+        print(f"No available seats for {person}")
+        
+    
