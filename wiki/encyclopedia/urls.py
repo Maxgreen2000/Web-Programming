4b6e347f7c1929c8347e302a3d5d0b88f97f0769
Maxgreen2000 <maxwell.green070100@gmail.com>
2022-12-28 23:12:47 +0000

    can now edit entries

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -5,4 +5,8 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:title>", views.viewentry, name="viewentry"),
+    path("search/", views.search, name="search"),
+    path("createpage/", views.createpage, name="createpage"),
+    path("editpage/", views.editpage, name="editpage"),
+    path("saveedit/", views.saveedit, name="saveedit"),
 ]
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -5,7 +5,8 @@ from . import util
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entries": util.list_entries(),
+        "pagetitle": "Search Results"
     })
 
 def viewentry(request, title):
@@ -20,3 +21,85 @@ def viewentry(request, title):
         "title": title,
         "content": content
     }) 
+
+def search(request):
+    if request.method == "POST":
+        searchquery = request.POST['q']
+        content = util.get_entry(searchquery)
+        if content == None:
+            searchresults = []
+            allentries = util.list_entries()
+            for entry in allentries:
+                    if searchquery.lower() in entry.lower():
+                        searchresults.append(entry)
+            return render(request, "encyclopedia/index.html", {
+                "entries": searchresults,
+                "pagetitle": "Search Results"
+            })
+        else:
+            return render(request, "encyclopedia/entry.html", {
+                "title": searchquery,
+                "content": content
+            }) 
+
+    else: 
+        pass
+
+
+def createpage(request):
+    if request.method == "POST":
+        if not request.POST['title']:
+            return render(request, "encyclopedia/error.html", {
+                "message": "Error: New Page Must Have a Title"
+            }) 
+        if not request.POST['body']:
+            return render(request, "encyclopedia/error.html", {
+                "message": "Error: New Page Must Have a Body"
+            }) 
+        else:
+            title = request.POST['title']
+            body = request.POST['body']
+            allentries = util.list_entries()
+            for entry in allentries:
+                if title.lower() ==  entry.lower():
+                    return render(request, "encyclopedia/error.html", {
+                        "message": "Error: Entry Exists of The Same Title"
+                    }) 
+            else:
+                util.save_entry(title, body)
+                return render(request, "encyclopedia/entry.html", {
+                    "title": title,
+                    "content": body
+                })
+            
+
+
+    else:
+        return render(request, "encyclopedia/createpage.html")
+
+def editpage(request):
+    if request.method == "POST":
+        title = request.POST.get('edittitle')
+        content = request.POST.get('editbody')
+        return render(request, "encyclopedia/editpage.html", {
+            "title": title,
+            "content": content
+        })
+    else:
+        return render(request, "encyclopedia/error.html", {
+            "message": "Error: GO FIND A PAGE TO EDIT FIRST"
+        })
+
+def saveedit(request):
+    if request.method == "POST":
+        title = request.POST.get('edittitle')
+        content = request.POST.get('editcontent')
+        util.save_entry(title, content)
+        return render(request, "encyclopedia/entry.html", {
+            "title": title,
+            "content": content
+        })
+    else:
+        return render(request, "encyclopedia/error.html", {
+            "message": "Error: GO FIND A PAGE TO EDIT FIRST"
+        })
