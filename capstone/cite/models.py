1e23c6bd292a2c0bdcd5106b8d602fd0810afb89
Maxgreen2000 <maxwell.green070100@gmail.com>
2023-01-08 23:54:14 +0000

    Model to store articles

--- capstone/cite/models.py
+++ capstone/cite/models.py
@@ -3,3 +3,16 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    publisher = models.CharField(blank=True, max_length=255)
+    day = models.CharField(blank=True, max_length=255)
+    month = models.CharField(blank=True, max_length=255)
+    year = models.CharField(max_length=255)
+    journal = models.CharField(blank=True, max_length=255)
+    content = models.TextField()
+
+    def __str__(self):
+        return f"{self.title}, {self.author}"
