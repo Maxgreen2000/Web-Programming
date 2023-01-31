751b7e19ed747864cae877c1b6614017cb96a491
Maxgreen2000 <maxwell.green070100@gmail.com>
2023-01-31 23:04:06 +0000

    project started and some work done

--- capstone/wmc/models.py
+++ capstone/wmc/models.py
@@ -1,3 +1,30 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+class User(AbstractUser):
+    pass
+
 # Create your models here.
+class Manuscript(models.Model):
+    summary = models.CharField(max_length=250)
+    location = models.CharField(max_length=250)
+    day = models.IntegerField(null=True)
+    month = models.IntegerField(null=True)
+    year = models.IntegerField(null=True)
+    tags = models.TextField()
+    transcript = models.TextField()
+
+    def __str__(self):
+        return f"{self.summary}, {self.location}, {self.year}"
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "summary": self.summary,
+            "location": self.location,
+            "day": self.day,
+            "month": self.month,
+            "year": self.year,
+            "tags": self.tags,
+            "transcript": self.transcript,
+        }
